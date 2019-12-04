2c00197a3a920d34d3ef60ef3436103ff1c3c21a
Rene <36115705+rene78@users.noreply.github.com>
2019-12-04 19:54:28 +0100

    First shot for proper frontend

--- js/main.js
+++ js/main.js
@@ -16,9 +16,13 @@ const contractAddress = "0x7f8b9483b79f735C34820497A1a7f9FB82C9224b";//Contract 
 // const contractAddress = "7855c451eE02CA17B4e2C08B628D5445FbF3dc6b";//Contract address on Ganache
 let provider;
 let signer;
+const swissFranc = three();
 
+//load all relevant infos in order to interact with Ethereum
 window.addEventListener('load', loadWeb3());
+//Get current ETH-fiat exchange rate from Cryptocompare
 window.addEventListener('load', getEthFiatRate());
+//Launch play() when user clicks on play button
 document.getElementById("form").addEventListener("submit", (event) => {
   event.preventDefault();
   //Find out which radio button is selected and how much money is bet.
@@ -27,14 +31,15 @@ document.getElementById("form").addEventListener("submit", (event) => {
   // console.log("0 or 1: " + headsOrTailsSelection);
   // console.log("Amount to bet (ETH): " + amountToBetEther);
   play(headsOrTailsSelection, amountToBetEther);
-}); //Launch play() when user clicks on play button
+});
+//Calculate fiat value during input of bet amount and show on page
 document.getElementById("amount-to-bet").addEventListener("input", () => {
   const amountToBetEther = document.querySelector("#amount-to-bet").value;
-  const betInDollar = document.querySelector("#bet-in-dollar");
   // console.log(amountToBetEther);
-  // console.log(ethUsd);
-  betInDollar.innerText = calcFiat(amountToBetEther);
-}); //Calculate value in USD during input
+  document.querySelector("#bet-in-dollar").innerText = calcFiat(amountToBetEther);
+  document.querySelector("#bet-in-eth2").innerText = amountToBetEther * 2;
+  document.querySelector("#bet-in-dollar2").innerText = calcFiat(amountToBetEther) * 2;
+});
 
 async function loadWeb3() {
   // Connect to the network
@@ -155,14 +160,18 @@ async function getLatestGameData() {
   for (let i = gameCount - 1; i >= 0; i--) {
     const gameEntry = await headsOrTails.getGameEntry(i);
     let result = gameEntry.winner ? "Won" : "Lost";
+    let resultClass = gameEntry.winner ? "won" : "lost";//define class to color text red or green
+    // console.log(resultClass);
     let guess = gameEntry.guess == 0 ? "Heads" : "Tails";
     //Shorten player address
     const addressShortened = gameEntry.addr.slice(0, 3) + "..." + gameEntry.addr.slice(-3);
     td[0].textContent = addressShortened;
-    td[1].textContent = ethers.utils.formatEther(gameEntry.amountBet) + " ETH";
+    td[1].textContent = ethers.utils.formatEther(gameEntry.amountBet);
     td[2].textContent = guess;
     td[3].textContent = result;
-    td[4].textContent = ethers.utils.formatEther(gameEntry.ethInJackpot) + " ETH";
+    td[3].className = "";//remove old class first
+    td[3].classList.add(resultClass);
+    td[4].textContent = ethers.utils.formatEther(gameEntry.ethInJackpot);
 
     let tb = document.querySelector("#table-body");
     let clone = document.importNode(t.content, true);
@@ -200,6 +209,136 @@ function calcFiat(etherToConvert) {
   // console.log(etherToConvert);
   // console.log(ethUsd);
   return (etherToConvert * ethUsd).toFixed(2);
+}
+
+function three() {
+  let scene, camera, renderer, coin, id, angleToVertical;
+  initializeScene();
+
+  // Adding ambient lighting
+  scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+
+  // Left point light
+  const pointLightLeft = new THREE.PointLight(0xff4422, 1);
+  pointLightLeft.position.set(-1, -1, 3);
+  scene.add(pointLightLeft);
+
+  // Right point light
+  const pointLightRight = new THREE.PointLight(0x44ff88, 1);
+  pointLightRight.position.set(1, 2, 3);
+  scene.add(pointLightRight);
+
+  // Top point light
+  const pointLightTop = new THREE.PointLight(0xdd3311, 1);
+  pointLightTop.position.set(0, 3, 2);
+  scene.add(pointLightTop);
+
+  THREE.ImageUtils.crossOrigin = '';
+  const textureCirc = new THREE.TextureLoader().load("img/circumference.jpg");
+  textureCirc.wrapS = THREE.RepeatWrapping;//repeat texture horizontally
+  textureCirc.repeat.set(20, 0);//repeat 20x
+  const textureHeads = new THREE.TextureLoader().load("img/heads.jpg");
+  const textureTails = new THREE.TextureLoader().load("img/tails.jpg");
+  const metalness = 0.7;
+  const roughness = 0.3;
+
+  const materials = [
+    new THREE.MeshStandardMaterial({
+      //Circumference
+      map: textureCirc,
+      metalness: metalness,
+      roughness: roughness
+    }),
+    //1st side
+    new THREE.MeshStandardMaterial({
+      map: textureHeads,
+      metalness: metalness,
+      roughness: roughness
+    }),
+    //2nd side
+    new THREE.MeshStandardMaterial({
+      map: textureTails,
+      metalness: metalness,
+      roughness: roughness
+    })
+  ];
+
+  var geometry = new THREE.CylinderGeometry(3, 3, 0.4, 100);
+  coin = new THREE.Mesh(geometry, materials);
+
+  scene.add(coin);
+  camera.position.set(0, 0, 7);
+
+  coin.rotation.x = Math.PI / 2;
+  coin.rotation.y = Math.PI / 2;
+
+  animateCoin();
+
+  //Update canvas on container size change. Thanks to gman (https://stackoverflow.com/a/45046955/5263954)!
+  function resizeCanvasToDisplaySize() {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    if (canvas.width !== width || canvas.height !== height) {
+      console.log("Container size of coin animation has changed. Canvas size updated!");
+      // you must pass false here or three.js sadly fights the browser
+      renderer.setSize(width, height, false);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+
+      // set render target sizes here
+    }
+  }
+
+  function animateCoin(time) {
+    time *= 0.001;  // seconds, not used
+    // console.log(time);
+
+    resizeCanvasToDisplaySize();
+
+    coin.rotation.x += 0.05;
+
+    renderer.render(scene, camera);
+    id = requestAnimationFrame(animateCoin);
+  }
+
+  function initializeScene() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(55, 1, 1, 10);
+    renderer = new THREE.WebGLRenderer({ canvas: document.querySelector(".result-coin canvas") });
+    scene.background = new THREE.Color(0x000000);
+  }
+
+  function stopAnimation(side) {
+    //on first run of function "side" is a string ("heads" or "tails"). On subsequent runs it is a timestamp (number)
+    if (typeof side === "string") {
+      // console.log(side);
+      angleToVertical = (side === "tails") ? (3 * Math.PI / 2) : (Math.PI / 2);//tails=1.5pi, heads=pi/2
+      // console.log(angleToVertical);
+    }
+
+    let rotVal = coin.rotation.x;
+
+    // console.log(rotVal);
+    let deltaAngle = rotVal % (Math.PI * 2) - angleToVertical;
+    // console.log(deltaAngle);
+
+    if (deltaAngle < 0.06 && deltaAngle > -0.06) {
+      cancelAnimationFrame(id);//cancel coin animation
+      cancelAnimationFrame(stopAnimation);//cancel excution of this function
+      //show start button
+      let startButton = document.querySelector(".start-button");
+      if (startButton) startButton.className = ""; //remove display: none, if not already done
+      return;
+    }
+    requestAnimationFrame(stopAnimation);//rerun this function until if-statment above is true
+  }
+
+  const coinObj = {
+    stopAnimation,
+    animateCoin
+  }
+  return coinObj;
 }
 
 // ---------------------------Temporary stuff---------------------------
